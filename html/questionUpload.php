ed18232858db3c6e9556b4beaf0fab016866f329
5up3rN00b <aaron.linear@gmail.com>
2020-06-08 16:03:40 -0700

    Added mysql commands

--- html/questionUpload.php
+++ html/questionUpload.php
@@ -1,4 +1,9 @@
 <?php
+$db = setupDb();
+if (!$db) {
+    echo "Database could not load";
+}
+
 $name = explode(".",  basename($_FILES['questionInput']['name']));
 
 $uploadFile = 'questions/' . $_FILES['questionInput']['name'];
@@ -6,13 +11,20 @@ $targetFolder = 'questions/' . $name[0];
 
 if (isset($_FILES['questionInput'])) {
     if (move_uploaded_file($_FILES['questionInput']['tmp_name'], $uploadFile)) {
-        `unzip $uploadFile -d `;
+        `unzip $uploadFile -d $targetFolder`;
+        `rm $uploadFile`;
+
+        $sth = $db->prepare("INSERT INTO `questions` (`name`, `difficulty`) VALUES (?, ?);");
+        $sth->execute([$name[0], $_POST['difficulty']]);
+
+        echo 'Successfully uploaded';
     } else {
         echo 'Unable to move file';
     }
 }
 ?>
 <form method="post" action="questionUpload.php" enctype="multipart/form-data">
+    <input name="difficulty" type="number" />
     Send this file: <input name="questionInput" type="file" />
     <input type="submit" value="Send File" />
 </form>
