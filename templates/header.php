9eabda97ab76987bf67736752d98b0a17facfdd0
5up3rN00b <aaron.linear@gmail.com>
2020-06-08 15:53:53 -0700

    Added libraries

--- templates/header.php
+++ templates/header.php
@@ -6,9 +6,9 @@ require("helper.php");
 <head>
     <meta charset="UTF-8">
     <title>CompCS Codes</title>
-    <script src="../html/libs/jquery-3.5.1.min.js"></script>
-    <script src="../html/libs/anime.min.js"></script>
-    <link rel="icon" href="../html/media/compcs.svg">
-    <link rel="stylesheet" href="../stylesheets/styles.css">
+    <script src="libs/jquery-3.5.1.min.js"></script>
+    <script src="libs/anime.min.js"></script>
+    <link rel="icon" href="media/compcs.svg">
+    <link rel="stylesheet" href="stylesheets/styles.css">
 </head>
 <body>
