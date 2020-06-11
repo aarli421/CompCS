5338ce90f724f60964485efc7378c6f5321e03d4
5up3rN00b <aaron.linear@gmail.com>
2020-06-10 18:02:21 -0700

    Added names to questionPostInput.php

--- html/questionPostInput.php
+++ html/questionPostInput.php
@@ -1,6 +1,11 @@
 <?php
 if (isset($_POST['input'])) {
     $dir = "users/test";
+    $name = $_POST['name'];
+    `mkdir $dir/$name`;
+
+    $dir .= "/" . $name;
+
     $dirAmount = `ls $dir | wc -l`;
     $amount = (int) ($dirAmount);
     $fileNum = $amount + 1;
