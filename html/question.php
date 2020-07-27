15d073b195b6faa0d65d571d6a5d849a3d39fa3b
Aaron <35825663+5up3rN00b@users.noreply.github.com>
2020-07-26 21:13:32 -0700

    Added center for starting upload

--- html/question.php
+++ html/question.php
@@ -16,6 +16,7 @@ $passArr = $sth->fetchAll();
 <script>
     function startUpload(){
         $(function() {
+            $("#prompt-center").css("text-align", "center");
             $('#dialogDiv').html("<div id=\"upload_process\" class=\"loader triangle\"> <svg viewBox=\"0 0 86 80\"><polygon points=\"43 8 79 72 7 72\"></polygon></svg></div>");
         });
     }
