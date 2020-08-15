614d4ca60b776f14ea9c7a00cbd80c73aaea12cf
Aaron <35825663+5up3rN00b@users.noreply.github.com>
2020-08-15 13:33:17 -0700

    Moved login register css

--- templates/header.php
+++ templates/header.php
@@ -11,13 +11,13 @@
     <meta name="keywords" content="">
     <meta name="author" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <link rel="stylesheet" href="css/login-register.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/font-awesome.min.css">
     <link rel="stylesheet" href="css/animate.css">
     <link rel="stylesheet" href="css/owl.carousel.css">
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
     <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/login-register.css">
     <link rel="stylesheet" href="css/home.css">
 
     <!-- MAIN CSS -->
