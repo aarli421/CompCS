06ca8a1b5b99e4d9e93561ab560ca0e84cdfbd18
Aaron <35825663+5up3rN00b@users.noreply.github.com>
2020-08-15 12:31:20 -0700

    Changed some parts of verification

--- html/verify.php
+++ html/verify.php
@@ -8,54 +8,6 @@ if (!$db) {
     echo "Database could not load";
 }
 ?>
-<script>
-    let redirect = "https://www.compcs.codes/index.php";
-<?php
-if (hasValue($_GET['email']) && hasValue($_GET['hash'])) {
-    $sth = $db->prepare("SELECT `username`, `email`, `hash`, `active` FROM `users` WHERE `email`=? AND `hash`=? AND `active`=0");
-    $sth->execute([$_GET['email'], $_GET['hash']]);
-    $passArr = $sth->fetchAll();
-
-    if (empty($passArr)) {
-//        echo "Account not found!";
-        ?>
-        $("#title").html("Your account was not found!");
-        redirect = "https://www.compcs.codes/register.php";
-    <?php
-    } else {
-        if ($passArr[0]['active'] == 1) {
-            echo "Your account has already been made";
-            ?>
-            $("#title").html("Your account was already verified!");
-            redirect = "https://www.compcs.codes/login.php";
-            <?php
-        } else {
-            $username = $passArr[0]['username'];
-            $msg = `mkdir users/$username`;
-
-            if (hasValue($msg)) {
-                ?>
-                $("#title").html("The verification process encountered an error!");
-                redirect = "https://www.compcs.codes/index.php#contact";
-                <?php
-            } else {
-                $sth = $db->prepare("UPDATE users SET active=1 WHERE email=? AND hash=? AND active=0");
-                $sth->execute([$_GET['email'], $_GET['hash']]);
-                ?>
-                $("#title").html("Your account was successfully verified!");
-                redirect = "https://www.compcs.codes/login.php";
-                <?php
-            }
-//            echo "Account Verified!";
-        }
-    }
-}
-?>
-    $("#message").html("Redirecting you in 3 seconds... If you are not redirected you can click <a href='" + redirect + "'>this</a>.");
-    setTimeout(function () {
-        $(location).attr("href", redirect);
-    }, 3000);
-</script>
 <style>
     .background {
         background-image: url("images/bestbg.png");
@@ -80,5 +32,53 @@ if (hasValue($_GET['email']) && hasValue($_GET['hash'])) {
         </div>
     </section>
 </div>
+<script>
+    let redirect = "http://www.compcs.codes/index.php";
+<?php
+if (hasValue($_GET['email']) && hasValue($_GET['hash'])) {
+    $sth = $db->prepare("SELECT `username`, `email`, `hash`, `active` FROM `users` WHERE `email`=? AND `hash`=?");
+    $sth->execute([$_GET['email'], $_GET['hash']]);
+    $passArr = $sth->fetchAll();
+
+    if (empty($passArr)) {
+//        echo "Account not found!";
+        ?>
+        $("#title").html("Your account was not found!");
+        redirect = "http://www.compcs.codes/register.php";
+    <?php
+    } else {
+        if ($passArr[0]['active'] == 1) {
+            echo "Your account has already been made";
+            ?>
+            $("#title").html("Your account was already verified!");
+            redirect = "http://www.compcs.codes/login.php";
+            <?php
+        } else {
+            $username = $passArr[0]['username'];
+            $msg = `mkdir users/$username`;
+
+            if (hasValue($msg)) {
+                ?>
+                $("#title").html("The verification process encountered an error!");
+                redirect = "http://www.compcs.codes/index.php#contact";
+                <?php
+            } else {
+                $sth = $db->prepare("UPDATE users SET active=1 WHERE email=? AND hash=? AND active=0");
+                $sth->execute([$_GET['email'], $_GET['hash']]);
+                ?>
+                $("#title").html("Your account was successfully verified!");
+                redirect = "http://www.compcs.codes/login.php";
+                <?php
+            }
+//            echo "Account Verified!";
+        }
+    }
+}
+?>
+    $("#message").html("Redirecting you in 3 seconds... If you are not redirected you can click <a href='" + redirect + "'>this</a>.");
+    setTimeout(function () {
+        $(location).attr("href", redirect);
+    }, 3000);
+</script>
 <?php
 require '../templates/footer.php';
