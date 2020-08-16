ff54c499d02bcaa3839fb2336ca2e2b7e5b8aea0
Aaron <35825663+5up3rN00b@users.noreply.github.com>
2020-08-15 20:00:12 -0700

    Added contact functionality

--- html/contact.php
+++ html/contact.php
@@ -1,5 +1,29 @@
 <?php
 require '../templates/helper.php';
+
+if (hasValue($_POST['name']) && hasValue($_POST['email']) && hasValue($_POST['subject']) && hasValue($_POST['message'])){
+    $handle = fopen('../private/keys.csv', 'r');
+    $data = fgetcsv($handle, 5, ',');
+
+    $email = new \SendGrid\Mail\Mail();
+    $email->setFrom("support@compcs.codes", $_POST['name']);
+    $email->setSubject($_POST['subject']);
+    $email->addTo("compcscodes@gmail.com", "CompCS Administrators");
+    $email->addContent(
+        "text/html", "From " . $_POST['email'] . "<br>" . $_POST['message']
+    );
+    $sendgrid = new \SendGrid($data[1]);
+    try {
+        $response = $sendgrid->send($email);
+        if ($response->statusCode() == 202) {
+
+        }
+    } catch(Exception $e) {
+
+    }
+    redirect("home");
+}
+
 require '../templates/header.php';
 ?>
     <!-- CONTACT -->
@@ -16,7 +40,7 @@ require '../templates/header.php';
                     </div>
 
                     <!-- CONTACT FORM -->
-                    <form action="#" method="post" class="wow fadeInUp" id="contact-form" role="form" data-wow-delay="0.8s">
+                    <form action="contact.php" method="post" class="wow fadeInUp" id="contact-form" role="form" data-wow-delay="0.8s">
 
                         <!-- IF MAIL SENT SUCCESSFUL  // connect this with custom JS -->
                         <h6 class="text-success">Your message has been sent successfully.</h6>
@@ -25,18 +49,18 @@ require '../templates/header.php';
                         <h6 class="text-danger">E-mail must be valid and message must be longer than 1 character.</h6>
 
                         <div class="col-md-6 col-sm-6">
-                            <input type="text" class="form-control" id="cf-name" name="name" placeholder="Full name">
+                            <input type="text" class="form-control" id="cf-name" name="name" placeholder="Full name" required>
                         </div>
 
                         <div class="col-md-6 col-sm-6">
-                            <input type="email" class="form-control" id="cf-email" name="email" placeholder="Email address">
+                            <input type="email" class="form-control" id="cf-email" name="email" placeholder="Email address" required>
                         </div>
 
                         <div class="col-md-12 col-sm-12">
-                            <input type="text" class="form-control" id="cf-subject" name="subject" placeholder="Subject">
+                            <input type="text" class="form-control" id="cf-subject" name="subject" placeholder="Subject" required>
 
                             <textarea class="form-control" rows="6" id="cf-message" name="message"
-                                      placeholder="Tell us about a problem or ask a question"></textarea>
+                                      placeholder="Tell us about a problem or ask a question" required></textarea>
 
                             <button type="submit" class="form-control" id="cf-submit" name="submit">Send Message</button>
                         </div>
