48d7e8f8dcd796b429b9aad286909d6f30288df1
Aaron <35825663+5up3rN00b@users.noreply.github.com>
2020-08-17 18:17:36 -0700

    Added new ui

--- html/home.php
+++ html/home.php
@@ -17,6 +17,7 @@ $points = $passArr[0]['points'];
 
 require '../templates/header.php';
 ?>
+<link rel="stylesheet" href="css/progress.css">
 <!-- Greeting Message -->
 <section data-stellar-background-ratio="0.5" style="padding-bottom: 25px;">
     <div class="container">
@@ -24,37 +25,94 @@ require '../templates/header.php';
         <h3 class="problemtitle" style="margin-top: 0px">Points: <?php echo $points; ?></h3>
     </div>
 </section>
-<!-- Form-->
-<section data-stellar-background-ratio="0.5" style="padding-top: 10px;">
-<div class="container">
-<table class="question">
-<tr class="categories">
-    <th class="problemnum">Problem #</th>
-    <th>Problem name</th>
-    <th>Points / Testcase</th>
-    <th class="link">Link</th>
-</tr>
-<?php
-$sth = $db->prepare("SELECT * FROM `questions` ORDER BY `unlock_value`");
-$sth->execute();
-$passArr = $sth->fetchAll();
-
-$i = 0;
-foreach ($passArr as $value) {
-    if ($value['unlock_value'] <= $points) {
-        $i++;
-        ?>
-        <tr class="categories">
-            <td><?php echo $i; ?></td>
-            <td><?php echo $value['name']; ?></td>
-            <td><?php echo $value['testcase_value']; ?></td>
-            <td><form method="get" action="question"><input name="questionName" value="<?php echo $value['name']; ?>" hidden=""><button type="submit" class="section-btn" style="margin:20px;">Go to question</button></form></td>
-        </tr>
-<?php   }
-} ?>
-</table>
-</div>
+<section data-stellar-background-ratio="0.5">
+    <div>
+        <ol>
+            <div style="padding-top: 5%"><hr style="height:10px; width: 85%; border-width:0; float: left; color:gray; background-color:gray"></div>
+            <li class="questions">
+                <div>
+                    <br>
+                    <br>
+                    <h2 class="division">Points: 1000 - 2000</h2>
+                </div>
+                <a href="">
+                    <div style="width: 80%; display: table; padding-top: 1%">
+                        <div><hr style="height:2px; width: 262%; border-width:0; color:gray; background-color:gray"></div>
+                        <div style="display: table-row; height: 100px;">
+                            <div style="width: 33%; display: table-cell">
+                                <h4>Problem: Where am I</h4>
+                            </div>
+                            <div style="width: 33%; height: 65%; display: table-cell">
+                                <h4>Unlock Value: 10</h4>
+                            </div>
+                            <div style="width: 33%; display: table-cell">
+                                <h4>Points/Testcase: 10</h4>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+                <div style="width: 70%; display: table">
+                    <div class="progress-wrap progress" data-progress-percent="90">
+                        <div class="progress-bar progress"></div>
+                    </div>
+                </div>
+                <a href="">
+                    <div style="width: 80%; display: table; padding-top: 1%">
+                        <div><hr style="height:2px; width: 262%; border-width:0; color:gray; background-color:gray"></div>
+                        <div style="display: table-row; height: 100px;">
+                            <div style="width: 33%; display: table-cell">
+                                <h4>Swapity-Swapity Swap</h4>
+                            </div>
+                            <div style="width: 33%; height: 65%; display: table-cell">
+                                <h4>Unlock Value: 10</h4>
+                            </div>
+                            <div style="width: 33%; display: table-cell">
+                                <h4>Points/Testcase: 10</h4>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+                <div style="width: 70%; display: table">
+                    <div class="progress-wrap progress" data-progress-percent="90">
+                        <div class="progress-bar progress"></div>
+                    </div>
+                </div>
+            </li>
+        </ol>
+    </div>
 </section>
+<!---->
+<!--<section data-stellar-background-ratio="0.5" style="padding-top: 10px;">-->
+<!--<div class="container">-->
+<!--<table class="question">-->
+<!--<tr class="categories">-->
+<!--    <th class="problemnum">Problem #</th>-->
+<!--    <th>Problem name</th>-->
+<!--    <th>Points / Testcase</th>-->
+<!--    <th class="link">Link</th>-->
+<!--</tr>-->
+<?php
+//$sth = $db->prepare("SELECT * FROM `questions` ORDER BY `unlock_value`");
+//$sth->execute();
+//$passArr = $sth->fetchAll();
+//
+//$i = 0;
+//foreach ($passArr as $value) {
+//    if ($value['unlock_value'] <= $points) {
+//        $i++;
+//        ?>
+<!--        <tr class="categories">-->
+<!--            <td>--><?php //echo $i; ?><!--</td>-->
+<!--            <td>--><?php //echo $value['name']; ?><!--</td>-->
+<!--            <td>--><?php //echo $value['testcase_value']; ?><!--</td>-->
+<!--            <td><form method="get" action="question"><input name="questionName" value="--><?php //echo $value['name']; ?><!--" hidden=""><button type="submit" class="section-btn" style="margin:20px;">Go to question</button></form></td>-->
+<!--        </tr>-->
+<?php //  }
+//} ?>
+<!--</table>-->
+<!--</div>-->
+<!--</section>-->
+<script src="js/progress.js"></script>
 <?php
 require '../templates/footer.php';
 ?>
