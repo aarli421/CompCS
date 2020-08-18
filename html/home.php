da3e418eec1f00399d90b8408574af2677688e2c
Aaron <35825663+5up3rN00b@users.noreply.github.com>
2020-08-17 18:36:25 -0700

    Moved stuff

--- html/home.php
+++ html/home.php
@@ -1,19 +1,19 @@
 <?php
-require '../templates/helper.php';
-
-if (hasValue($_POST['logout'])) {
-    unset($_SESSION['user']);
-}
-
-if (!isset($_SESSION['user'])) {
-    redirect("login");
-    exit();
-}
-
-$sth = $db->prepare("SELECT `points` FROM `users` WHERE `username`=?");
-$sth->execute([$_SESSION['user']]);
-$passArr = $sth->fetchAll();
-$points = $passArr[0]['points'];
+//require '../templates/helper.php';
+//
+//if (hasValue($_POST['logout'])) {
+//    unset($_SESSION['user']);
+//}
+//
+//if (!isset($_SESSION['user'])) {
+//    redirect("login");
+//    exit();
+//}
+//
+//$sth = $db->prepare("SELECT `points` FROM `users` WHERE `username`=?");
+//$sth->execute([$_SESSION['user']]);
+//$passArr = $sth->fetchAll();
+//$points = $passArr[0]['points'];
 
 require '../templates/header.php';
 ?>
@@ -29,54 +29,61 @@ require '../templates/header.php';
     <div>
         <ol>
             <div style="padding-top: 5%"><hr style="height:10px; width: 85%; border-width:0; float: left; color:gray; background-color:gray"></div>
-            <li class="questions">
+            <li>
                 <div>
                     <br>
                     <br>
                     <h2 class="division">Points: 1000 - 2000</h2>
                 </div>
-                <a href="">
-                    <div style="width: 80%; display: table; padding-top: 1%">
-                        <div><hr style="height:2px; width: 262%; border-width:0; color:gray; background-color:gray"></div>
-                        <div style="display: table-row; height: 100px;">
-                            <div style="width: 33%; display: table-cell">
-                                <h4>Problem: Where am I</h4>
-                            </div>
-                            <div style="width: 33%; height: 65%; display: table-cell">
-                                <h4>Unlock Value: 10</h4>
-                            </div>
-                            <div style="width: 33%; display: table-cell">
-                                <h4>Points/Testcase: 10</h4>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-                <div style="width: 70%; display: table">
-                    <div class="progress-wrap progress" data-progress-percent="90">
-                        <div class="progress-bar progress"></div>
-                    </div>
-                </div>
-                <a href="">
-                    <div style="width: 80%; display: table; padding-top: 1%">
-                        <div><hr style="height:2px; width: 262%; border-width:0; color:gray; background-color:gray"></div>
-                        <div style="display: table-row; height: 100px;">
-                            <div style="width: 33%; display: table-cell">
-                                <h4>Swapity-Swapity Swap</h4>
-                            </div>
-                            <div style="width: 33%; height: 65%; display: table-cell">
-                                <h4>Unlock Value: 10</h4>
-                            </div>
-                            <div style="width: 33%; display: table-cell">
-                                <h4>Points/Testcase: 10</h4>
+            </li>
+            <ol>
+                <li class="questions">
+
+                    <a href="">
+                        <div style="width: 80%; display: table; padding-top: 1%">
+                            <div><hr style="height:2px; width: 262%; border-width:0; color:gray; background-color:gray"></div>
+                            <div style="display: table-row; height: 100px;">
+                                <div style="width: 33%; display: table-cell">
+                                    <h4>Problem: Where am I</h4>
+                                </div>
+                                <div style="width: 33%; height: 65%; display: table-cell">
+                                    <h4>Unlock Value: 10</h4>
+                                </div>
+                                <div style="width: 33%; display: table-cell">
+                                    <h4>Points/Testcase: 10</h4>
+                                </div>
                             </div>
                         </div>
+                    </a>
+                    <div style="width: 70%; display: table">
+                        <div class="progress-wrap progress" data-progress-percent="90">
+                            <div class="progress-bar progress"></div>
+                        </div>
                     </div>
-                </a>
-                <div style="width: 70%; display: table">
-                    <div class="progress-wrap progress" data-progress-percent="90">
-                        <div class="progress-bar progress"></div>
+                </li>
+                <li class="questions">
+                    <a href="">
+                        <div style="width: 80%; display: table; padding-top: 1%">
+                            <div><hr style="height:2px; width: 262%; border-width:0; color:gray; background-color:gray"></div>
+                            <div style="display: table-row; height: 100px;">
+                                <div style="width: 33%; display: table-cell">
+                                    <h4>Swapity-Swapity Swap</h4>
+                                </div>
+                                <div style="width: 33%; height: 65%; display: table-cell">
+                                    <h4>Unlock Value: 10</h4>
+                                </div>
+                                <div style="width: 33%; display: table-cell">
+                                    <h4>Points/Testcase: 10</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                    <div style="width: 70%; display: table">
+                        <div class="progress-wrap progress" data-progress-percent="90">
+                            <div class="progress-bar progress"></div>
+                        </div>
                     </div>
-                </div>
+            </ol>
             </li>
         </ol>
     </div>
