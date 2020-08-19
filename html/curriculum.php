7f3e7055e1b7e46ce41d5301aca922b70a85f207
Aaron <35825663+5up3rN00b@users.noreply.github.com>
2020-08-18 22:57:04 -0700

    Added div

--- html/curriculum.php
+++ html/curriculum.php
@@ -3,78 +3,80 @@ require '../templates/helper.php';
 require '../templates/header.php';
 ?>
 <section>
-    <center>
+    <div class="container">
         <h1>Curriculum</h1>
-    </center>
-    <div class="curriculum">
-        <div class="curriculum-div">
-            <div class="topics">
-                <ol>
-                    <li>
-                        <h2>Division 1</h2>
-                        <ul>
-                            <li class="bullet">
-                                Big O Notation
-                            </li>
-                            <li class="bullet">
-                                File IO
-                            </li>
-                            <li class="bullet">
-                                Variables, Arithmetic, Primitive Data Types
-                            </li>
-                            <li class="bullet">
-                                Conditional Statements
-                            </li>
-                            <li class="bullet">
-                                Loops
-                            </li>
-                            <li class="bullet">
-                                Arrays &#38; Matrices
-                            </li>
-                            <li class="bullet">
-                                Functions
-                            </li>
-                            <li class="bullet">
-                                OOP (Object Oriented Programming)
-                                <ul>
-                                    <li>
-                                        Creating a class and objects
-                                    </li>
-                                    <br>
-                                    <li>
-                                        Constructors
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <h2>Division 2</h2>
-                        <ul>
-                            <li class="bullet">
-                                Math
-                            </li>
-                            <li class="bullet">
-                                String Processing
-                            </li>
-                            <li class="bullet">
-                                Loops
-                            </li>
-                            <li class="bullet">
-                                Sliding Window / Pointers
-                            </li>
-                            <li class="bullet">
-                                Data Structures (Queue, Stack)
-                            </li>
-                            <li class="bullet">
-                                Graph Representation &#38; Traversal (DFS, BFS)
-                            </li>
-                            <li class="bullet">
-                                Greedy Algorithm
-                            </li>
-                        </ul>
-                    </li>
-                </ol>
+    </div>
+    <div class="container">
+        <div class="curriculum">
+            <div class="curriculum-div">
+                <div class="topics">
+                    <ol>
+                        <li>
+                            <h2>Division 1</h2>
+                            <ul>
+                                <li class="bullet">
+                                    Big O Notation
+                                </li>
+                                <li class="bullet">
+                                    File IO
+                                </li>
+                                <li class="bullet">
+                                    Variables, Arithmetic, Primitive Data Types
+                                </li>
+                                <li class="bullet">
+                                    Conditional Statements
+                                </li>
+                                <li class="bullet">
+                                    Loops
+                                </li>
+                                <li class="bullet">
+                                    Arrays &#38; Matrices
+                                </li>
+                                <li class="bullet">
+                                    Functions
+                                </li>
+                                <li class="bullet">
+                                    OOP (Object Oriented Programming)
+                                    <ul>
+                                        <li>
+                                            Creating a class and objects
+                                        </li>
+                                        <br>
+                                        <li>
+                                            Constructors
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h2>Division 2</h2>
+                            <ul>
+                                <li class="bullet">
+                                    Math
+                                </li>
+                                <li class="bullet">
+                                    String Processing
+                                </li>
+                                <li class="bullet">
+                                    Loops
+                                </li>
+                                <li class="bullet">
+                                    Sliding Window / Pointers
+                                </li>
+                                <li class="bullet">
+                                    Data Structures (Queue, Stack)
+                                </li>
+                                <li class="bullet">
+                                    Graph Representation &#38; Traversal (DFS, BFS)
+                                </li>
+                                <li class="bullet">
+                                    Greedy Algorithm
+                                </li>
+                            </ul>
+                        </li>
+                    </ol>
+                </div>
             </div>
         </div>
     </div>
