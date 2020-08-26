ddeb082d03a12b67bb883da836b6d92cea4dab41
Aaron <35825663+5up3rN00b@users.noreply.github.com>
2020-08-25 18:08:10 -0700

    Changed verification email

--- html/forgot.php
+++ html/forgot.php
@@ -7,27 +7,34 @@ if (hasValue($_POST['forgotEmail'])) {
 
     $hash = md5(rand(0, 10000));
 }
+
+if (hasValue($_GET['hash']) && hasValue($_GET['email'])) {
 ?>
-<div class="background">
-    <section data-stellar-background-ratio="0.5">
-        <div class="container">
-            <div class="form">
-                <div class="form-panel one">
-                    <div class="form-header">
-                        <h1>Forgot Password</h1>
+
+<?php
+} else {
+?>
+    <div class="background">
+        <section data-stellar-background-ratio="0.5">
+            <div class="container">
+                <div class="form">
+                    <div class="form-panel one">
+                        <div class="form-header">
+                            <h1>Forgot Password</h1>
+                        </div>
+                        <div class="form-content">
+                            <form id="forgot" method="post" action="forgot">
+                                <div class="form-group"><label for="forgotEmail">Email</label><input type="email" id="forgotEmail" name="forgotEmail" required="required" /></div>
+                                <div class="form-group"><button form="login" type="submit">Request Change</button></div>
+                            </form>
+                        </div>
+                        <h6 id="forgotSuccess" class="text-success"></h6>
+                        <h6 id="forgotError" class="text-danger"></h6>
                     </div>
-                    <div class="form-content">
-                        <form id="forgot" method="post" action="forgot">
-                            <div class="form-group"><label for="forgotEmail">Username</label><input type="email" id="forgotEmail" name="forgotEmail" required="required" /></div>
-                            <div class="form-group"><button form="login" type="submit">Request Change</button></div>
-                        </form>
-                    </div>
-                    <h6 id="forgotSuccess" class="text-success"></h6>
-                    <h6 id="forgotError" class="text-danger"></h6>
                 </div>
             </div>
-        </div>
-    </section>
-</div>
+        </section>
+    </div>
 <?php
+}
 require '../templates/footer.php';
