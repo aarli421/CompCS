f8f0f49f8d7a1a7b3bb0ce55b105d35c57b1f758
Aaron <35825663+5up3rN00b@users.noreply.github.com>
2020-09-03 09:56:33 -0700

    Added links to youtube and compcs

--- templates/footer.php
+++ templates/footer.php
@@ -22,8 +22,6 @@
                     <address class="wow fadeInUp" data-wow-delay="0.4s">
 <!--                        <p>090-080-0650 | 090-070-0430</p>-->
                         <p><a href="mailto:compcscodes@gmail.com">compcscodes@gmail.com</a></p>
-                        <p><a href="https://www.tinyurl.com/compcscodes" target="_blank">Facebook Page</a></p>
-                        <p><a href="https://www.tinyurl.com/compcscodesyoutube" target="_blank">Youtube</a></p>
 <!--                        <p>LINE: eatery247 </p>-->
                     </address>
                 </div>
@@ -52,8 +50,8 @@
 
                 <div class="wow fadeInUp copyright-text" data-wow-delay="0.8s">
                     <ul class="wow fadeInUp social-icon animated">
-                        <li><a href="" class="fa fa-facebook-square"></a></li>
-                        <li><a href="" class="fa fa-youtube-square"></a></li>
+                        <li><a href="https://www.tinyurl.com/compcscodes" class="fa fa-facebook-square"></a></li>
+                        <li><a href="https://www.tinyurl.com/compcscodesyoutube" class="fa fa-youtube-square"></a></li>
                     </ul>
                     <p>Copyright &copy; 2020 <br>CompCS</p>
 
