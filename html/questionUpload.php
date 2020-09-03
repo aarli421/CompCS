cb9570ac2efc0f715249f4cffc46547d45018f48
Aaron <35825663+5up3rN00b@users.noreply.github.com>
2020-09-03 13:39:12 -0700

    Added upload

--- html/questionUpload.php
+++ html/questionUpload.php
@@ -16,23 +16,23 @@ if (isset($_FILES['questionInput']) && isset($_POST['unlock_value']) && isset($_
         `sudo $scriptsDirectory/executeAsUser.sh questionsadmin "unzip $uploadFile -d $targetFolder; rm $uploadFile"`;
 
         $ioDirAmount = `ls $targetFolder | wc -l`;
-        $testAmount = ((int) ($ioDirAmount - 1)) / 2;
+        $testAmount = ((int) ($ioDirAmount)) / 2;
 
-        $sth = $db->prepare("INSERT INTO `questions` (`name`, `unlock_value`, `testcase_value`, `testcases`) VALUES (?, ?, ?, ?);");
-        $sth->execute([$name[0], $_POST['unlock_value'], $_POST['testcase_value'], $testAmount]);
+        $sth = $db->prepare("INSERT INTO `questions` (`name`, `prompt`, `unlock_value`, `testcase_value`, `testcases`) VALUES (?, ?, ?, ?, ?);");
+        $sth->execute([$name[0], $_POST['prompt'], $_POST['unlock_value'], $_POST['testcase_value'], $testAmount]);
         ?>
         <script>
             $("#dialogDiv").html("Successfully uploaded. Don't refresh and confirm submission or else the question will be duplicated!");
         </script>
         <?php
-        echo 'Successfully uploaded';
+//        echo 'Successfully uploaded';
     } else {
         ?>
         <script>
             $("#dialogDiv").html("Unable to move file. Don't refresh and confirm submission or else it will be duplicated!");
         </script>
         <?php
-        echo 'Unable to move file';
+//        echo 'Unable to move file';
     }
 }
 
