dc64845aec7d1916355ed5b37082ee32de9c8024
Aaron <35825663+5up3rN00b@users.noreply.github.com>
2020-09-08 09:42:08 -0700

    Added leaderboard

---
+++ html/leaderboard.php
@@ -0,0 +1,52 @@
+<?php
+require '../templates/helper.php';
+require '../templates/header.php';
+?>
+<link rel="stylesheet" href="css/leaderboard.css">
+<section>
+    <center>
+        <h1>Leaderboard</h1>
+    </center>
+    <div class="table">
+
+        <div class="table-cell">
+
+            <ul class="leader" style="padding-bottom: 20%">
+                <li>
+                    <span class="listTop">1</span>
+                    <h2 class="top">Drew Vosburg<span class="numberTop">9,654</span></h2>
+                </li>
+                <li>
+                    <span class="listTop">2</span>
+                    <h2 class="top">Jesse Nelson<span class="numberTop">8,364</span</h2>
+                </li>
+                <li>
+                    <span class="listTop">3</span>
+                    <h2 class="top">Joel Bullis<span class="numberTop">7,621</span</h2>
+                </li>
+                <li>
+                    <span class="list_num">4</span>
+                    <h2>Carmen SanDiego<span class="number">4,582</span</h2>
+                </li>
+                <li>
+                    <span class="list_num">5</span>
+                    <h2>Charles Barkley<span class="number">2,651</span</h2>
+                </li>
+                <li>
+                    <span class="list_num">5</span>
+                    <h2>Charles Barkley<span class="number">2,651</span</h2>
+                </li>
+                <li>
+                    <span class="list_num">5</span>
+                    <h2>Charles Barkley<span class="number">2,651</span</h2>
+                </li>
+                <li>
+                    <span class="list_num">5</span>
+                    <h2>Charles Barkley<span class="number">2,651</span</h2>
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
+<?php
+require '../templates/footer.php';
