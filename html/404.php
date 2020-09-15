0307735ff34d9b9ebd905f31aca1aaa83acc7153
Aaron <35825663+5up3rN00b@users.noreply.github.com>
2020-09-15 15:39:06 -0700

    Added 403 and 404

---
+++ html/404.php
@@ -0,0 +1,25 @@
+<?php
+require '../templates/helper.php';
+require '../templates/header.php';
+?>
+<section data-stellar-background-ratio="0.5">
+    <div class="container">
+        <div class="form">
+            <div class="form-toggle"></div>
+            <div class="form-panel one">
+                <center>
+                    <div class="form-header">
+
+                        <img src="images/404.svg" alt="404" width="200" height="200" style="padding-bottom: 17px">
+                        <h1>404</h1>
+                        <h2>This is not the web page you are looking for.</h2>
+                    </div>
+                    <p> Click <a href = "index"> here </a> to go back to the main page.</p>
+                </center>
+            </div>
+
+        </div>
+    </div>
+</section>
+<?php
+require '../templates/footer.php';
