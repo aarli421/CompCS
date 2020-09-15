ac1d5052e6bed453714f41be9c67cc0337be016e
Aaron <35825663+5up3rN00b@users.noreply.github.com>
2020-09-15 15:46:27 -0700

    Added background image

--- html/403.php
+++ html/403.php
@@ -2,22 +2,24 @@
 require '../templates/helper.php';
 require '../templates/header.php';
 ?>
-<section data-stellar-background-ratio="0.5">
-    <div class="container">
-        <div class="form">
-            <div class="form-toggle"></div>
-            <div class="form-panel one">
-                <center>
-                    <div class="form-header">
-                        <img src="images/403.svg" alt="403" width="200" height="200" style="padding-bottom: 17px">
-                        <h1>403</h1>
-                        <h2>The page you are trying to access is forbidden.</h2>
-                    </div>
-                    <p> Click <a href = "index"> here </a> to go back to the home screen.</p>
-                </center>
+<div class="background">
+    <section data-stellar-background-ratio="0.5">
+        <div class="container">
+            <div class="form">
+                <div class="form-toggle"></div>
+                <div class="form-panel one">
+                    <center>
+                        <div class="form-header">
+                            <img src="images/403.svg" alt="403" width="200" height="200" style="padding-bottom: 17px">
+                            <h1>403</h1>
+                            <h2>The page you are trying to access is forbidden.</h2>
+                        </div>
+                        <p> Click <a href="index"> here </a> to go back to the home screen.</p>
+                    </center>
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+</div>
 <?php
 require '../templates/footer.php';
--- html/404.php
+++ html/404.php
@@ -2,24 +2,26 @@
 require '../templates/helper.php';
 require '../templates/header.php';
 ?>
-<section data-stellar-background-ratio="0.5">
-    <div class="container">
-        <div class="form">
-            <div class="form-toggle"></div>
-            <div class="form-panel one">
-                <center>
-                    <div class="form-header">
+<div class="background">
+    <section data-stellar-background-ratio="0.5">
+        <div class="container">
+            <div class="form">
+                <div class="form-toggle"></div>
+                <div class="form-panel one">
+                    <center>
+                        <div class="form-header">
 
-                        <img src="images/404.svg" alt="404" width="200" height="200" style="padding-bottom: 17px">
-                        <h1>404</h1>
-                        <h2>This is not the web page you are looking for.</h2>
-                    </div>
-                    <p> Click <a href = "index"> here </a> to go back to the main page.</p>
-                </center>
+                            <img src="images/404.svg" alt="404" width="200" height="200" style="padding-bottom: 17px">
+                            <h1>404</h1>
+                            <h2>This is not the web page you are looking for.</h2>
+                        </div>
+                        <p> Click <a href="index"> here </a> to go back to the main page.</p>
+                    </center>
+                </div>
+
             </div>
-
         </div>
-    </div>
-</section>
+    </section>
+</div>
 <?php
 require '../templates/footer.php';
