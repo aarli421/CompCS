e7562f9a04d09410d96d70cbbd8f613a6596bb57
Aaron <35825663+5up3rN00b@users.noreply.github.com>
2020-09-17 09:19:13 -0700

    Added youtube embed

--- html/index.php
+++ html/index.php
@@ -216,13 +216,14 @@ require '../templates/header.php';
                     </div>
 
                     <div class="wow fadeInUp" data-wow-delay="0.4s">
-                        <p>After registering for an account, go to your email and confirm your account. (Make sure to read all the terms of service)</p>
-                        <p>When you log into your account, you will arrive at the homepage. You can see your current points at the top.
-                            Each problem on the website has an unlock value and a total point value. </p>
-                        <p>When you click on one of the unlocked problems, you will see the problem and a place to submit your code. After uploading your file,
-                            our system will automatically check multiple test cases to ensure that your code satisfies all test cases. The bar above the problem
-                            will show you the number of test cases you have completed as well as the ones you have not. You can continue uploading code until all
-                            test cases satisfy your code. For each test case completed you get a percentage of the total points the question is worth.</p>
+                        <iframe allowfullscreen="" allow="autoplay; encrypted-media" frameborder="0" src="https://www.youtube.com/embed/nsxNZFj6sTk" height="630" width="1120"></iframe>
+<!--                        <p>After registering for an account, go to your email and confirm your account. (Make sure to read all the terms of service)</p>-->
+<!--                        <p>When you log into your account, you will arrive at the homepage. You can see your current points at the top.-->
+<!--                            Each problem on the website has an unlock value and a total point value. </p>-->
+<!--                        <p>When you click on one of the unlocked problems, you will see the problem and a place to submit your code. After uploading your file,-->
+<!--                            our system will automatically check multiple test cases to ensure that your code satisfies all test cases. The bar above the problem-->
+<!--                            will show you the number of test cases you have completed as well as the ones you have not. You can continue uploading code until all-->
+<!--                            test cases satisfy your code. For each test case completed you get a percentage of the total points the question is worth.</p>-->
                     </div>
                 </div>
             </div>
