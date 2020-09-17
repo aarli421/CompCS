3dd17a5dc098498befb61df89b46158ff94f6b2b
Aaron <35825663+5up3rN00b@users.noreply.github.com>
2020-09-16 18:49:20 -0700

    Changed back id to test

--- html/ajax/upload.php
+++ html/ajax/upload.php
@@ -167,12 +167,12 @@ if (!hasValue($arr['error']) && hasValue($date)) {
     $sth = $db->prepare("INSERT INTO submissions (`user_id`, `question_id`, `submission`, `timestamp`) VALUES (?, ?, ?, ?)");
     $sth->execute([$user_id, $question[0]['question_id'], $fileVal, $date]);
 
-//    $sth = $db->prepare("SELECT LAST_INSERT_ID();");
-//    $sth->execute();
-//    $id = $sth->fetchAll();
+    $sth = $db->prepare("SELECT LAST_INSERT_ID();");
+    $sth->execute();
+    $id = $sth->fetchAll();
 
-    $sth = $db->prepare("INSERT INTO grades (`user_id`, `question_id`, `output_json`, `correct_cases`, `timestamp`) VALUES (?, ?, ?, ?, ?)");
-    $sth->execute([$user_id, $question[0]['question_id'], json_encode($arr), $arr['correct_cases'], $date]);
+    $sth = $db->prepare("INSERT INTO grades (`user_id`, `question_id`, `submission_id`, `output_json`, `correct_cases`, `timestamp`) VALUES (?, ?, ?, ?, ?, ?)");
+    $sth->execute([$user_id, $question[0]['question_id'], $id[0][0], json_encode($arr), $arr['correct_cases'], $date]);
 
     $sth = $db->prepare("COMMIT;");
     $sth->execute();
