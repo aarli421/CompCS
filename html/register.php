ac6fb48ef583e5d5caea342672593c83a62f6c8e
Aaron <35825663+5up3rN00b@users.noreply.github.com>
2020-10-09 16:01:11 -0700

    Added register

--- html/register.php
+++ html/register.php
@@ -6,38 +6,47 @@ $sth = $db->prepare("SELECT * FROM schools");
 $sth->execute();
 $schools = $sth->fetchAll();
 ?>
-<style>
-    .alert {
-        position: relative;
-        padding: 20px;
-        background-color: #f44336;
-        color: white;
-        opacity: 1;
-        transition: opacity 0.6s;
-        margin-bottom: 15px;
-        z-index: 999;
-    }
-
-    .alert.success {background-color: #4CAF50;}
-    .alert.info {background-color: #2196F3;}
-    .alert.warning {background-color: #ff9800;}
-
-    .closebtn {
-        margin-left: 15px;
-        color: white;
-        font-weight: bold;
-        float: right;
-        font-size: 22px;
-        line-height: 20px;
-        cursor: pointer;
-        transition: 0.3s;
-        z-index: 9999;
-    }
-
-    .closebtn:hover {
-        color: black;
-    }
-</style>
+<div class="background">
+    <!-- Form-->
+    <section data-stellar-background-ratio="0.5">
+        <div class="container">
+            <div class="form">
+                <div class="form-panel one">
+                    <div class="form-header">
+                        <h1>Register Account</h1>
+                    </div>
+                    <div class="form-content">
+                        <form id="register" method="post">
+                            <div class="form-group"><label for="registerUsername">Username (Only letters and numbers)</label><input type="text" id="registerUsername" name="signUpUsername" required="required" pattern="<?php echo $usernameReg; ?>" /></div>
+                            <div class="form-group"><label for="registerEmail">Email Address</label><input type="email" id="registerEmail" name="signUpEmail" required="required" pattern="<?php echo $emailReg; ?>" /></div>
+                            <div class="form-group"><label for="registerPassword">Password (>8 letters, numbers, and symbols)</label><input type="password" id="registerPassword" name="signUpPassword" required="required" pattern="<?php echo $passwordReg; ?>" /></div>
+                            <div class="form-group"><label for="registerCPassword">Confirm Password</label><input type="password" id="registerCPassword" name="signUpCPassword" required="required" /></div>
+                            <div class="form-group school"><label>School</label></div>
+                            <div class="form-group">
+                                <ul class="list-unstyled">
+                                    <li class="init item">Select School</li>
+                                    <?php
+                                    $len = count($schools);
+                                    for ($i = 0; $i < $len; $i++) {
+                                    ?>
+                                    <li class="item" data-value="value <?php echo $i + 1; ?>" style="<?php if ($i == 0) { ?>padding-top: 10% <?php } ?>"><?php echo $schools[$i]['name']; ?></li>
+                                    <?php
+                                    }
+                                    ?>
+                                </ul>
+                            </div>
+                            <div class="form-group agree after"><label class="form-remember"><input type="checkbox" name="tos" checked="checked" required="required"/>Agree to <a href="tos" target="_blank">Terms of Service</a></label></div>
+                            <input id="registerSchool" name="signUpSchool" type="hidden" value="" required>
+                            <div class="form-group after"><button id="registerBtn" form="register" type="submit">Register</button></div>
+                        </form>
+                    </div>
+                    <h6 id="registerSuccess" class="text-success"></h6>
+                    <h6 id="registerError" class="text-danger"></h6>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
 <script>
     function clear() {
         $("#registerUsername").val("");
@@ -101,49 +110,7 @@ $schools = $sth->fetchAll();
             });
         });
     });
-</script>
-<div class="background">
-    <!-- Form-->
-    <section data-stellar-background-ratio="0.5">
-        <div class="container">
-            <div class="form">
-                <div class="form-panel one">
-                    <div class="form-header">
-                        <h1>Register Account</h1>
-                    </div>
-                    <div class="form-content">
-                        <form id="register" method="post">
-                            <div class="form-group"><label for="registerUsername">Username (Only letters and numbers)</label><input type="text" id="registerUsername" name="signUpUsername" required="required" pattern="<?php echo $usernameReg; ?>" /></div>
-                            <div class="form-group"><label for="registerEmail">Email Address</label><input type="email" id="registerEmail" name="signUpEmail" required="required" pattern="<?php echo $emailReg; ?>" /></div>
-                            <div class="form-group"><label for="registerPassword">Password (>8 letters, numbers, and symbols)</label><input type="password" id="registerPassword" name="signUpPassword" required="required" pattern="<?php echo $passwordReg; ?>" /></div>
-                            <div class="form-group"><label for="registerCPassword">Confirm Password</label><input type="password" id="registerCPassword" name="signUpCPassword" required="required" /></div>
-                            <div class="form-group school"><label>School</label></div>
-                            <div class="form-group">
-                                <ul class="list-unstyled">
-                                    <li class="init item">Select School</li>
-                                    <?php
-                                    $len = count($schools);
-                                    for ($i = 0; $i < $len; $i++) {
-                                    ?>
-                                    <li class="item" data-value="value <?php echo $i + 1; ?>" style="<?php if ($i == 0) { ?>padding-top: 10% <?php } ?>"><?php echo $schools[$i]['name']; ?></li>
-                                    <?php
-                                    }
-                                    ?>
-                                </ul>
-                            </div>
-                            <div class="form-group agree after"><label class="form-remember"><input type="checkbox" name="tos" checked="checked" required="required"/>Agree to <a href="tos" target="_blank">Terms of Service</a></label></div>
-                            <input id="registerSchool" name="signUpSchool" type="hidden" value="" required>
-                            <div class="form-group after"><button id="registerBtn" form="register" type="submit">Register</button></div>
-                        </form>
-                    </div>
-                    <h6 id="registerSuccess" class="text-success"></h6>
-                    <h6 id="registerError" class="text-danger"></h6>
-                </div>
-            </div>
-        </div>
-    </section>
-</div>
-<script>
+
     $(".list-unstyled").on("click", ".init", function() {
         $(this).closest(".list-unstyled").children('.item:not(.init)').toggle();
     });
