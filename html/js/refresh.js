4db29de2b7eee1e6f94019fc6057814d2afa4d75
5up3rN00b <aaron.linear@gmail.com>
2020-10-25 16:09:28 -0700

    Changed to interval

--- html/js/refresh.js
+++ html/js/refresh.js
@@ -1,15 +1,10 @@
-var refreshSession = function () {
-    var time = 600000; // 10 mins
-    setTimeout(
-        function () {
-            $.ajax({
-                url: 'ajax/refresh.php',
-                cache: false,
-                complete: function () {
-                    refreshSession();
-                }
-            });
-        },
-        time
-    );
-};
+var time = 600000; // 10 mins
+setInterval(
+    function () {
+        $.ajax({
+            url: 'ajax/refresh.php',
+            cache: false
+        });
+    },
+    time
+);
