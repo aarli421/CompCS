522e6cc46b72c015617d6648b1ed394b723102da
5up3rN00b <aaron.linear@gmail.com>
2020-10-26 09:25:11 -0700

    Removed the weird part

--- html/verify.php
+++ html/verify.php
@@ -56,10 +56,10 @@ if (hasValue($_GET['email']) && hasValue($_GET['hash'])) {
             //$msg2 = `sudo $scriptsDirectory/addAdmin.sh $username`;
 
             //if (hasValue($msg) || hasValue($msg2)) {
-                //?>
+                //
                 //$("#title").html("The verification process encountered an error!");
                 //redirect = "https://www.compcs.codes/register";
-                //<?php
+                //
             //} else {
                 $sth = $db->prepare("UPDATE users SET active=1 WHERE email=? AND hash=? AND active=0");
                 $sth->execute([$_GET['email'], $_GET['hash']]);
