2de9d5fd6e4c228f394d7b2b05b1572645abc1a2
5up3rN00b <aaron.linear@gmail.com>
2020-10-25 22:53:14 -0700

    Added email message

--- html/register.php
+++ html/register.php
@@ -14,6 +14,7 @@ $schools = $sth->fetchAll();
                 <div class="form-panel one">
                     <div class="form-header">
                         <h1>Register Account</h1>
+                        <h6 class="text-desc">Our emailing system is temporarily down. Please register later, sorry for the inconvenience!</h6>
                     </div>
                     <div class="form-content">
                         <form id="register" method="post">
@@ -110,6 +111,8 @@ $schools = $sth->fetchAll();
             });
         });
     });
+
+
 
     $(".list-unstyled").on("click", ".init", function() {
         $(this).closest(".list-unstyled").children('.item:not(.init)').toggle();
