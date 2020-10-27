543397bb3fceeadccc6e045b128729c1fdeda907
5up3rN00b <aaron.linear@gmail.com>
2020-10-26 17:45:16 -0700

    Changed so it will output all divisions

--- html/ajax/update.php
+++ html/ajax/update.php
@@ -1,1 +1,27 @@
 <?php
+require '../../templates/helper.php';
+
+if (hasValue($_POST['discord'])) {
+    $sth = $db->prepare("SELECT * FROM `users` WHERE `discord`=?");
+    $sth->execute([$_POST['discord']]);
+    $user = $sth->fetchAll();
+
+    if (!empty($user)) {
+        $s = $user[0]['school'];
+
+        $sth = $db->prepare("SELECT * FROM `divisions` WHERE `bonus`=0");
+        $sth->execute();
+        $divisions = $sth->fetchAll();
+
+        foreach ($divisions as $id => $division) {
+            if ($user[0]['points'] >= $division[0]['lower']) {
+                $s .= "|" . $division[0]['name'];
+            }
+        }
+
+        echo $s;
+
+    } else {
+        echo "You have not linked your discord account yet.";
+    }
+}
