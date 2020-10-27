d5c94e3c738b35f4a47c56da66ff8f7cda20c3ff
5up3rN00b <aaron.linear@gmail.com>
2020-10-27 14:13:37 -0700

    Removed refresh from question

--- html/question.php
+++ html/question.php
@@ -110,7 +110,6 @@ if ($access) {
 ?>
 <link rel="stylesheet" href="css/question.css">
 <link rel="stylesheet" href="css/loader.css">
-<script src="js/refresh.js"></script>
 <script>
     function startUpload(){
         $(function() {
