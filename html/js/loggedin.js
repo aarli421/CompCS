c35c698c18c89ea21fa80675abc5570a94ac5aa6
5up3rN00b <aaron.linear@gmail.com>
2020-10-27 15:28:06 -0700

    login

--- html/js/loggedin.js
+++ html/js/loggedin.js
@@ -1,15 +1,8 @@
-var time = 60000; // 1 min
-setInterval(
-    function () {
-        $.ajax({
-            url: 'ajax/loggedin.php',
-            cache: false,
-            success: function(data) {
-                if (data == "Not logged in") {
-                    $(location).attr("href", "https://www.compcs.codes/login");
-                }
-            }
-        });
-    },
-    time
-);
+var time = 60000;
+setInterval(function(){$.post('https://www.compcs.codes/ajax/loggedin', function (data) {
+    if (data == "Not logged in") {
+        $(location).attr("href", "https://www.compcs.codes/login");
+    }
+});
+}
+,time);
