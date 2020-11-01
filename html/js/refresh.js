04b65ee280716465a9a558acd17d2c434e91dae9
5up3rN00b <aaron.linear@gmail.com>
2020-10-31 18:36:06 -0700

    Changed to 5 min

--- html/js/refresh.js
+++ html/js/refresh.js
@@ -1,5 +1,5 @@
-// var time = 300000;
-var time = 60000;
+var time = 300000;
+// var time = 60000;
 setInterval(function(){$.get('https://www.compcs.codes/ajax/refresh.php', function (data) {
         console.log(data);
     });
