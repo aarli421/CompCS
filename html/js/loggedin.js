cdc7c8ce312b97afb32a8e7f85987dc31e5782e6
5up3rN00b <aaron.linear@gmail.com>
2020-10-31 17:17:48 -0700

    Changed refresh and loggedin

--- html/js/loggedin.js
+++ html/js/loggedin.js
@@ -1,4 +1,4 @@
-var time = 120000;
+var time = 600000;
 setInterval(function(){$.post('https://www.compcs.codes/ajax/loggedin.php', function (data) {
     if (data == "Not logged in") {
         $(location).attr("href", "https://www.compcs.codes/login");
--- html/js/refresh.js
+++ html/js/refresh.js
@@ -1,4 +1,4 @@
-var time = 120000;
+var time = 600000;
 setInterval(function(){$.post('https://www.compcs.codes/ajax/refresh.php', function (data) {
         console.log(data);
     });
