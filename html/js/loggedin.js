6d388396cab23922c9a4805e81fa3c868eef259c
5up3rN00b <aaron.linear@gmail.com>
2020-11-01 13:40:47 -0800

    Switched up the testing for being not loggedin and changed refresh to around 15 minutes

--- html/js/loggedin.js
+++ html/js/loggedin.js
@@ -1,9 +1,14 @@
 var time = 300000;
 // var time = 60000;
-setInterval(function(){$.get('https://www.compcs.codes/ajax/loggedin.php', function (data) {
-    if (data == "Not logged in") {
-        $(location).attr("href", "https://www.compcs.codes/login");
-    }
-});
-}
-,time);
+window.setInterval( function() {
+    $.ajax({
+        cache: false,
+        type: "GET",
+        url: "https://www.compcs.codes/ajax/loggedin.php",
+        success: function(data) {
+            if (data == "Not logged in") {
+                $(location).attr("href", "https://www.compcs.codes/login");
+            }
+        }
+    });
+}, time );
