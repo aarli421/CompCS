6609109880d468f88a050b47937434f216ecedfe
5up3rN00b <aaron.linear@gmail.com>
2020-10-31 18:55:47 -0700

    Changed back to ajax

--- html/js/refresh.js
+++ html/js/refresh.js
@@ -1,7 +1,12 @@
-var time = 300000;
-// var time = 60000;
-setInterval(function(){$.get('https://www.compcs.codes/ajax/refresh.php', function (data) {
-        console.log(data);
+// var time = 300000;
+var time = 60000;
+window.setInterval( function() {
+    $.ajax({
+        cache: false,
+        type: "GET",
+        url: "https://www.compcs.codes/ajax/refresh.php",
+        success: function(data) {
+            console.log(data);
+        }
     });
-}
-,time);
+}, time );
