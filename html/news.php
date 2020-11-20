121b43e5eb23ae884051b7e110bf96c13b40d9b2
5up3rN00b <aaron.linear@gmail.com>
2020-11-20 12:34:48 -0800

    Added news

--- html/news.php
+++ html/news.php
@@ -10,7 +10,11 @@ require '../templates/header.php';
 <section data-stellar-background-ratio="0.5">
     <div class="container">
         <div class="row">
-            Hello World!
+            <div class="col-md-12 col-sm-12">
+                <div class="section-title wow fadeInUp" data-wow-delay="0.1s">
+                    <h2>News</h2>
+                </div>
+            </div>
         </div>
     </div>
 </section>
