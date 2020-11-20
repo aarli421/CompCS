c691597a85e08b8e54f0df2fb12a2ef4668321eb
5up3rN00b <aaron.linear@gmail.com>
2020-11-20 12:09:45 -0800

    Added world

--- html/news.php
+++ html/news.php
@@ -2,6 +2,13 @@
 require '../templates/helper.php';
 require '../templates/header.php';
 ?>
+<section data-stellar-background-ratio="0.5">
+    <div class="container">
+        <div class="row">
+            Hello World!
+        </div>
+    </div>
+</section>
 <?php
 require '../templates/footer.php';
 ?>
